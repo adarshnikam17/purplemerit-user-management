ba282b171e3315a1a751220dbf6cf1d2cea33a24
Adarsh Nikam <adarshnikam256@gmail.com>
2025-12-30 13:32:56 +0530

    Add JWT auth middleware and protected user profile API

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 
@@ -9,6 +10,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+
 
 app.get("/", (req, res) => {
   res.json({ message: "PurpleMerit Backend API is running" });
